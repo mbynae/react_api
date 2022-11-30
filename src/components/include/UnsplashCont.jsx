3676b251192887c0fa341f66c724b90f7264df02
mybnae <piowm123@gmail.com>
2022-11-30 10:34:43 +0900

    asd

--- src/components/include/UnsplashCont.jsx
+++ src/components/include/UnsplashCont.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+/** @format */
 
+import React from "react";
 
-const UnsplashImage = ({image}) => {
-  return (
-    <li>
-        <a href={`https://unsplash.com/photos/${image.id}`}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-        </a>
-    </li>
-  );
+const UnsplashImage = ({ image }) => {
+    return (
+        <li>
+            <a href={`https://unsplash.com/photos/${image.id}`}>
+                <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+        </li>
+    );
 };
 
-const UnsplashCont = ({images}) => {
-  return (
-    <section className='cont__unsplash'>
-        <div className='container'>
-            <div className='unsplash__inner'>
-                <ul>
-                    {images.map((image, index) => (
-                        <UnsplashImage key={index} image={image}/>
-                    ))}
-                </ul>
+const UnsplashCont = ({ images }) => {
+    return (
+        <section className="cont__unsplash">
+            <div className="container">
+                <div className="unsplash__inner">
+                    <ul>
+                        {images.map((image, index) => (
+                            <UnsplashImage key={index} image={image} />
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
-    </section>
-  )
-}
+        </section>
+    );
+};
 
-export default UnsplashCont
+export default UnsplashCont;
